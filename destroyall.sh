017840aac0364148f040b72d8bcf564262bc416b
arecastudio <ondiisrail@gmail.com>
2024-02-27 14:35:48 +0800

    update: autodestroy

--- destroyall.sh
+++ destroyall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-ls *-cidata.iso 2>/dev/null|awk -F'-' '{print $1}'|while read vm;do ./autodestroy2.sh $vm;done
+ls *-cidata.iso 2>/dev/null|awk -F'-' '{print $1}'|while read vm;do ./autodestroy.sh $vm;done
 
 
 #echo "[]" > /var/lib/libvirt/dnsmasq/virbr0.status
